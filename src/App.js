5d19112433e21e920e6ee224315e984eaecc5709
Bill Liu <pengcheng.liu3@gmail.com>
2023-10-04 22:55:29 +1300

    add routing for react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // src/App.js
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TrialBalance from './components/TrialBalance';
-import ButtonComponent from './components/ButtonComponent';
+import TCodeAssign from './components/TCodeAssign';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Trial Balance</h1>
-            <TrialBalance />
-            <ButtonComponent />
-        </div>
+        <Router>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">TCode Assign</Link>
+                        </li>
+                        <li>
+                            <Link to="/trial-balance">Trial Balance</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<><h1>TCode Assign Page</h1><TCodeAssign /></>} />
+                    <Route path="/trial-balance" element={<><h1>Trial Balance</h1><TrialBalance /></>} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
