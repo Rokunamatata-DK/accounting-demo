547a1f8f27555d2b00eee5d98cb0582a2e738551
Bill Liu <pengcheng.liu3@gmail.com>
2023-10-04 22:58:12 +1300

    add TCode page

---
+++ src/components/TCodeAssign.js
@@ -0,0 +1,13 @@
+// src/components/TCodeAssign.js
+import React from 'react';
+
+const TCodeAssign = () => {
+    return (
+        <div>
+            {/* Your TCodeAssign component content here */}
+            <h2>Welcome to the TCode Assign Page!</h2>
+        </div>
+    );
+}
+
+export default TCodeAssign;
