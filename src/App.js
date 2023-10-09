7db49aa6faf99b1291486292136e5485d074e718
Bill Liu <pengcheng.liu3@gmail.com>
2023-10-09 14:22:13 +1300

    stylin gfor header

--- src/App.js
+++ src/App.js
@@ -7,59 +7,64 @@ import TCodeManager from './components/TCodeManager';
 
 function App() {
     const [tCodes, setTCodes] = useState([100, 110, 111, 200, 210, 300, 310, 400, 500]);
-    const [assignedTCodes, setAssignedTCodes] = useState({}); // State to store assigned TCodes for each transaction
+    const [assignedTCodes, setAssignedTCodes] = useState({});
 
     return (
         <Router>
             <div className="App">
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/transaction">Transaction</Link>
-                        </li>
-                        <li>
-                            <Link to="/trial-balance">Trial Balance</Link>
-                        </li>
-                        <li>
-                            <Link to="/tcode-manager">TCode Manager</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Routes>
-                    <Route path="/" element={
-                        <>
-                            <h1>Transaction</h1>
-                            <TCodeAssign
-                                tCodes={tCodes}
-                                assignedTCodes={assignedTCodes}
-                                setAssignedTCodes={setAssignedTCodes}
-                            />
-                        </>
-                    } />
-                    <Route path="/transaction" element={
-                        <>
-                            <h1>Transaction</h1>
-                            <TCodeAssign
-                                tCodes={tCodes}
-                                assignedTCodes={assignedTCodes}
-                                setAssignedTCodes={setAssignedTCodes}
-                            />
-                        </>
-                    } />
-                    <Route path="/trial-balance" element={
-                        <>
-                            <h1>Trial Balance</h1>
-                            <TrialBalance assignedTCodes={assignedTCodes} />
-                        </>
-                    } />
-                    <Route path="/tcode-manager" element={
-                        <>
-                            <h1>TCode Manager</h1>
-                            <TCodeManager tCodes={tCodes} setTCodes={setTCodes} />
-                        </>
-                    } />
-                </Routes>
+                <header className="app-header">
+                    <h1>Client Name</h1>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/transaction">Transaction</Link>
+                            </li>
+                            <li>
+                                <Link to="/trial-balance">Trial Balance</Link>
+                            </li>
+                            <li>
+                                <Link to="/tcode-manager">TCode Manager</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={
+                            <>
+                                <h2>Transaction</h2>
+                                <TCodeAssign
+                                    tCodes={tCodes}
+                                    assignedTCodes={assignedTCodes}
+                                    setAssignedTCodes={setAssignedTCodes}
+                                />
+                            </>
+                        } />
+                        <Route path="/transaction" element={
+                            <>
+                                <h2>Transaction</h2>
+                                <TCodeAssign
+                                    tCodes={tCodes}
+                                    assignedTCodes={assignedTCodes}
+                                    setAssignedTCodes={setAssignedTCodes}
+                                />
+                            </>
+                        } />
+                        <Route path="/trial-balance" element={
+                            <>
+                                <h2>Trial Balance</h2>
+                                <TrialBalance assignedTCodes={assignedTCodes} />
+                            </>
+                        } />
+                        <Route path="/tcode-manager" element={
+                            <>
+                                <h2>TCode Manager</h2>
+                                <TCodeManager tCodes={tCodes} setTCodes={setTCodes} />
+                            </>
+                        } />
+                    </Routes>
+                </div>
             </div>
         </Router>
     );
